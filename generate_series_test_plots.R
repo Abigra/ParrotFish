eb9e5055ce41591e826a83542d9b93e1c6e01b6e
Abigra <95701618+Abigra@users.noreply.github.com>
2021-12-16 18:03:01 -0500

    svaed abd updated

--- generate_series_test_plots.R
+++ generate_series_test_plots.R
@@ -4,14 +4,23 @@ d <- .035
 y<- .5
 dt<- .01
 
+g=.21
+  
+test_1 <-generate_series(.4,.4,g)
+test_2<- generate_series(.1,.1,g)
+test_3<- generate_series(.4,.1,g)
+test_4<- generate_series(.1,.4,g)
 
-test_1 <-generate_series(.4,.4,.21)
-test_2<- generate_series(.1,.1,.21)
-test_3<- generate_series(.4,.1,.21)
-test_4<- generate_series(.1,.4,.21)
-ggplot()+
+plot4<- ggplot()+
   geom_point(data=test_1,aes(x=v_M_values1, y=v_C_values1, color=".4,.4"))+
   geom_point(data=test_2, aes(x=v_M_values1, y=v_C_values1, color=".1,.1"))+
   geom_point(data=test_3, aes(x=v_M_values1, y=v_C_values1, color=".1,.4"))+
-  geom_point(data=test_4, aes(x=v_M_values1, y=v_C_values1, color=".4,.1"))
-  
+  geom_point(data=test_4, aes(x=v_M_values1, y=v_C_values1, color=".4,.1"))+
+  labs(x="Macroalgae Cover", y= "Coral Cover", title=paste(g))
+
+plot1
+plot2
+plot3 
+plot4
+
+save(plot1,plot2,plot3,plot4, file="simulated_test_trajectories.Rdata")
